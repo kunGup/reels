b65bb09e817d5bf4b8129801ad5bfaae487f60b2
Kunal Gupta <goodkunal723@gmail.com>
2021-11-18 21:05:57 +0530

    upload video

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,11 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../Context/AuthContext";
+import { database, storage } from "../firebase";
+import UploadFile from "./UploadFile";
 function Feed() {
-  const { logout } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
+  const [userData, setUserData] = useState("");
+  useEffect(() => {
+    const unsub = database.users.doc(user.uid).onSnapshot((snapshot) => {
+      setUserData(snapshot.data());
+    });
+    return () => {
+      unsub();
+    };
+  }, [user]);
   return (
-    <div>
-      <h1>welcome</h1>
-      <button onClick={logout}>logout</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div className="comp" style={{ width: "50%" }}>
+        <h1>welcome</h1>
+        <button onClick={logout}>logout</button>
+      </div>
+      <UploadFile user={userData} />
     </div>
   );
 }

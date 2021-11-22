be23d9af5934f6ca27187104f1b5c43e9175af2d
Kunal Gupta <goodkunal723@gmail.com>
2021-11-22 10:20:38 +0530

    ioa and hosting

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -45,6 +45,27 @@ function Posts({ userData }) {
       });
     return unsub;
   }, []);
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      let ele = entry.target.childNodes[0];
+      console.log(ele);
+      ele.play().then(() => {
+        if (!ele.paused && !entry.isIntersecting) {
+          ele.pause();
+        }
+      });
+    });
+  };
+  let observer = new IntersectionObserver(callback, { threshold: 0.6 });
+  useEffect(() => {
+    const elements = document.querySelectorAll(".videos");
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
+    return () => {
+      observer.disconnect();
+    };
+  }, [posts]);
   return (
     <div>
       {posts == null || userData == null ? (
